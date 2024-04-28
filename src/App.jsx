af9c43ab05350325e64794673ec650f378fff5e4
gladwin2802 <ajgladwin2802@gmail.com>
2024-04-28 23:32:14 +0530

    changed App.jsx Value path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
 import'./App.css';
 import Residencies from "./components/Residencies/Residencies";
-import Value from "./components/value/value";
+import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Footer from "./components/Footer/Footer";
